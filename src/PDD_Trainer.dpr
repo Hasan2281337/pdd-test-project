ad0f62c60dbce29f2bc9d6dba69167fce9197105
Hasan2281337 <90681441+Hasan2281337@users.noreply.github.com>
2022-12-26 23:29:39 +0700

    Add files via upload

--- src/PDD_Trainer.dpr
+++ src/PDD_Trainer.dpr
@@ -1,24 +1,23 @@
 ﻿program PDD_Trainer;
-
 uses
   Forms,
   UConfigBase in 'UConfigBase.pas',
   UConfigClient in 'UConfigClient.pas',
   UMainMenu in 'Forms\UMainMenu.pas' {FMainMenu},
   UResults in 'Forms\UResults.pas' {FResults},
-  UTrainer in 'Forms\UTrainer.pas' {FTrainer};
+  UTrainer in 'Forms\UTrainer.pas' {FTrainer},
+  Vcl.Themes,
+  Vcl.Styles,
+  UDataModule in 'UDataModule.pas' {DataModule1: TDataModule};
 
 {$R *.res}
-
 begin
   Application.Initialize;
   Application.Title := 'ПДД Тренажёр';
-
   Config := TAccessToFiles.Create();
-
   Application.CreateForm(TFMainMenu, FMainMenu);
   Application.CreateForm(TFResults, FResults);
   Application.CreateForm(TFTrainer, FTrainer);
-
+  Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
